8c299f017c5ff4cf213b47b6bc198f7d694c39ce
Veronika <mnika5437@gmail.com>
2025-03-18 15:45:47 +0200

    to fixed bugs in task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    return message.lenght <= maxLength ? message : message.slice(0, maxLength) + "...";
+    return message.length <= maxLength ? message : message.slice(0, maxLength) + "...";
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
